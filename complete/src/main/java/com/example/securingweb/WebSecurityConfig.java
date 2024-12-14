d9745946e6355cb7d241c6f197b6e2c4ccf28ccf
Subash <reach2subbu@gmail.com>
2024-12-14 12:12:38 -0600

    Login wiht any user implemented

--- complete/src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ complete/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,29 +17,41 @@ public class WebSecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
+		http.csrf((csrf) -> csrf.disable())
 			.authorizeHttpRequests((requests) -> requests
 				.requestMatchers("/", "/home").permitAll()
 				.anyRequest().authenticated()
 			)
 			.formLogin((form) -> form
 				.loginPage("/login")
+				// .successForwardUrl("/hello")
+				.defaultSuccessUrl("/hello")
 				.permitAll()
 			)
-			.logout((logout) -> logout.permitAll().logoutUrl("/logout").logoutSuccessUrl("/login?success"));
+			.logout((logout) -> logout.permitAll().logoutUrl("/logout").logoutSuccessUrl("/login?logout"));
 
 		return http.build();
 	}
 
 	@Bean
 	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
+		// UserDetails user =
+		// 	 User.withDefaultPasswordEncoder()
+		// 		.username("user")
+		// 		.password("password")
+		// 		.roles("USER")
+		// 		.build();
 
-		return new InMemoryUserDetailsManager(user);
+		return new UserDetailsService(){
+			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				// Always return a user with the given username and a fixed password 
+				return User.withDefaultPasswordEncoder() 
+					.username(username)
+					.password("password")
+					.roles("USER")
+					.build(); 
+			}
+		};
 	}
 }
+
